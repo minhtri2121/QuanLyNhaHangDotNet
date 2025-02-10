a314beef54a6f98e3e0de356b9acbef797e94f76
Đoàn Minh Trị <118013543+minhtri2121@users.noreply.github.com>
2025-02-11 03:00:44 +0700

    Diep (#17)
    * Diệp
    
    * done thay đổi thông tin cá nhân
    
    * tabindex account profile (#16)
    
    Co-authored-by: Trần Trung Kiên <trantrungkieen2809@gmail.com>
    
    ---------
    
    Co-authored-by: diep-ux <dieppro004@gmail.com>
    Co-authored-by: Trần Trung Kiên <trantrungkieen2809@gmail.com>

---
+++ QuanLyNhaHang/DAO/FoodDAO.cs
@@ -0,0 +1,51 @@
+﻿using QuanLyNhaHang.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaHang.DAO
+{
+    public class FoodDAO
+    {
+        private static FoodDAO instance;
+
+        public static FoodDAO Instance
+        {
+            get { if (instance == null) instance = new FoodDAO(); return FoodDAO.instance; }
+            private set { FoodDAO.instance = value; }
+        }
+
+        private FoodDAO() { }
+
+        public List<Food> GetFoodByCategoryID(int ID)
+        {
+            List<Food> list = new List<Food>();
+            string query = "select *\r\nfrom MON where IDNhomMon = '" + ID + "'";
+            DataTable data = DataProvider.Instance.ExcuteQuery(query);
+            foreach (DataRow item in data.Rows)
+            {
+                Food f = new Food(item);
+                list.Add(f);
+            }
+
+            return list;
+        }
+
+        public List<DVT> GetDVT()
+        {
+            List<DVT> list = new List<DVT>();
+            string query = "SELECT * FROM DON_VI_TINH";
+            DataTable data = DataProvider.Instance.ExcuteQuery(query);
+            foreach (DataRow item in data.Rows)
+            {
+                DVT d = new DVT(item);
+                list.Add(d);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ QuanLyNhaHang/DTO/Food.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaHang.DTO
+{
+    public class Food
+    {
+        public Food(int id, string name, string idnhom, string namenhom, int iddvt, int gia)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Idnhom = idnhom;
+            this.Namenhom = namenhom;
+            this.Iddvt = iddvt;
+            this.Tendvt = tendvt;
+            this.Gia = gia;
+        }
+
+        public Food(DataRow row)
+        {
+            this.Id = (int)row["IDMon"];
+            this.Name = row["TenMon"].ToString();
+            this.Idnhom = row["IDNhomMon"].ToString();
+            this.Iddvt = (int)row["IDDVT"];
+            this.Gia = (float)Convert.ToDouble(row["Gia"]);
+        }
+
+        private int id;
+        private string name;
+        private string idnhom;
+        private string namenhom;
+        private int iddvt;
+        private string tendvt;
+        private float gia;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name {
+            get { return name; }
+            set { name = value; } 
+        }
+        public string Idnhom
+        {
+            get { return idnhom; }
+            set { idnhom = value; }
+        }
+        public string Namenhom
+        {
+            get { return namenhom; }
+            set { namenhom = value; }
+        }
+        public int Iddvt
+        {
+            get { return iddvt ; }
+            set { iddvt  = value; }
+        }
+        public float Gia
+        {
+            get { return gia ; }
+            set { gia = value; }
+        }
+        public string Tendvt
+        {
+            get { return tendvt; }
+            set { tendvt = value; }
+        }
+    }
+}
